cd20258a9771fb72e8ead43d3158421e1c643253
Nadav Israeli <nadavisraeli@campus.technion.ac.il>
2026-03-15 11:38:04 +0200

    add source file

---
+++ source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void win() {
+	printf("You win! here is your flag");
+    system("/bin/cat flag.txt);
+}
+
+int main() {
+    int length;
+    char buf[64];
+
+	scanf("%d", &length);
+	if(length >= 64) {
+		printf("message too long");
+		return 0;
+	}
+
+	read(0, buf, length);
+	printf("%s", buf);
+
+	return 0;
+}
